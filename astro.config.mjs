f7016598e3a863bb568a4999291e2a63f9a2b02a
graicy <eq1der@gmail.com>
2025-12-22 00:31:14 +0200

    chore: update Astro configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  // 1. Повна адреса вашого сайту (БЕЗ назви репозиторію в кінці)
+  site: 'https://wlsro.github.io',
+
+  // 2. Назва вашого репозиторію з початковим слешем
+  // Наприклад, якщо репозиторій називається "baget-site", то '/baget-site'
+  base: '/calc_astro_v1',
   vite: {
     plugins: [tailwindcss()]
   }
